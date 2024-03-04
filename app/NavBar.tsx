7fb2ed5d81aea15cbde547024be4781b7d5f0408
“mariama-ali” <mariama.alii98@gmail.com>
2024-03-04 18:45:49 -0500

    Build NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,28 @@
+import { link } from 'fs'
+import Link from 'next/link'
+import React from 'react'
+import { AiFillBug } from 'react-icons/ai'
+
+const NavBar = () => {
+    const links = [
+        {lables: 'Dashboard', href: '/'},
+        {lables: 'Issues', href: '/issues'},
+    ]
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href="/"><AiFillBug/></Link>
+        <ul className='flex space-x-6'>
+            {links.map(link =>
+                <Link 
+                key={link.href}
+                className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                href={link.href}>{link.lables}
+                </Link>
+            )}
+        </ul>
+
+    </nav>
+  )
+}
+
+export default NavBar
