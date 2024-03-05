5cec88690a10331681f3dd57db1eaea85404591c
“mariama-ali” <mariama.alii98@gmail.com>
2024-03-05 18:06:53 -0500

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {Button} from '@radix-ui/themes'
 
 const page = () => {
   return (
-    <div>Issuespage</div>
+    <div><Button> New Issue </Button></div>
   )
 }
 
