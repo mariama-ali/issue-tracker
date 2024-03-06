f7d43b548e5d01a1e32f8485e6a5b37a7ea2b8d7
“mariama-ali” <mariama.alii98@gmail.com>
2024-03-05 19:06:44 -0500

    Customize Radix UI theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from "react";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
+
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
