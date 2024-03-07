69909c8bfd7208b128387a6a8dbde1643f190f23
“mariama-ali” <mariama.alii98@gmail.com>
2024-03-06 23:00:08 -0500

    Handle Form Submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,39 @@
 'use client'
 import React from "react";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
+import {useForm} from "react-hook-form";
+import axios from "axios";
+import 'easymde/dist/easymde.min.css';
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router =  useRouter();
+  const { register, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input
+          placeholder="Title"
+          {...register("title", { required: true })}
+        />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <TextArea
+        placeholder="Description"
+        {...register("description", { required: true })}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
